9375204954d1c7235922af76cff03a1478735286
serbolinatona-gif <serbolinatona@gmail.com>
2026-04-03 00:59:45 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import sqlite3
 from flask import Flask, render_template, request, redirect, session, g
 from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="templates",
+    static_folder="static"
+)
 app.secret_key = "vpered_secret_123"
 
 DATABASE = "vpered.db"
